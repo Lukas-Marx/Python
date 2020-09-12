194433342c58dff93b6cd9dfefa160ee2d2f16c5
Lukas-Marx <70298828+Lukas-Marx@users.noreply.github.com>
2020-09-13 00:34:14 +0200

    Lernprozess für Python

--- Aktien.py
+++ Aktien.py
@@ -2,6 +2,7 @@
 #Lukas.Marx@Fau.de
 import pandas as pd
 import yfinance as yf
+import numpy as np
 import matplotlib as matplot
 data = yf.download("AMZN AAPL GOOG", start="2017-01-01", end="2017-04-30")
 data
@@ -10,3 +11,5 @@ data2 = yf.download("AMZN AAPL GOOG", start="2017-01-01",
                     end="2017-04-30", group_by='tickers')
 data2
 
+print(data)
+print(data2)
--- Learning.py
+++ Learning.py
@@ -1,3 +1,16 @@
 #Lukas Marx, 11.09.2020 Fürth, Friedrich-Alexander-Universität Erlangen-Nuernberg
 #Lukas.Marx@Fau.de
-print("this line will be printed")
+x = 1
+if x == 1:
+    # intented four spaces
+    print ("x is", x, ".")
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world ="world"
+helloworld = hello + " "+world
+print(helloworld)
