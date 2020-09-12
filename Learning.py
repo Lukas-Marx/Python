05cc356a28ce4a245d04f090b72d4e239bc434a3
Lukas-Marx <70298828+Lukas-Marx@users.noreply.github.com>
2020-09-13 00:21:10 +0200

    Lernprozess für Python

--- Learning.py
+++ Learning.py
@@ -1,12 +1,3 @@
 #Lukas Marx, 11.09.2020 Fürth, Friedrich-Alexander-Universität Erlangen-Nuernberg
 #Lukas.Marx@Fau.de
-import pandas as pd
-import yfinance as yf
-import matplotlib as matplot
-data = yf.download("AMZN AAPL GOOG", start="2017-01-01", end="2017-04-30")
-data
-
-data2 = yf.download("AMZN AAPL GOOG", start="2017-01-01",
-                    end="2017-04-30", group_by='tickers')
-data2
-
+print("this line will be printed")
