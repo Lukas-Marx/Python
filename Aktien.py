721137f05c21235edfb19d856f83fdae75ebffde
Lukas-Marx <70298828+Lukas-Marx@users.noreply.github.com>
2021-04-30 20:18:59 +0200

    Lernprozess für Python

--- Aktien.py
+++ Aktien.py
@@ -4,12 +4,18 @@ import pandas as pd
 import yfinance as yf
 import numpy as np
 import matplotlib as matplot
-data = yf.download("AMZN AAPL GOOG", start="2017-01-01", end="2017-04-30")
-data
+data1 = yf.download("AMZN AAPL GOOG", start="2017-01-01", end="2017-04-30")
+data1
 
 data2 = yf.download("AMZN AAPL GOOG", start="2017-01-01",
                     end="2017-04-30", group_by='tickers')
 data2
 
-print(data)
+print(data1)
 print(data2)
+data = data1 + data2
+
+print (data)
+
+Amazon_stock = yf.download("AMZN", start="2020-09-01", end="2020-09-15")
+print(Amazon_stock)
