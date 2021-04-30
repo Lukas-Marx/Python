192402c1ab59f111dfbe3908e593923bfb2d0ee6
Lukas-Marx <70298828+Lukas-Marx@users.noreply.github.com>
2021-04-30 20:39:04 +0200

    Lerne Python

--- pythonfromthebeginning.py
+++ pythonfromthebeginning.py
@@ -1,3 +1,20 @@
-#Lukas Marx 03.05.1994
-#Lukas.Marx@FAU.de
-print ('just like this')
+# Lukas Marx 03.05.1994
+# Lukas.Marx@FAU.de
+print ('just like this') #String
+a = 1 + 2 * 3 #Berechnung
+print(a)
+print ("Can't use single quotation marks here!") #Integer
+
+print(12) #Integer
+print('12') #String
+
+b = 99 > 100
+print(b)
+c = 4+3+2+1 ==10 #wahr falsch abfrage ein ist gleich waere die belegung eines parameters
+print(c)
+print('a==b wahr wenn gleich'
+      'a<b wahr wenn a kleiner b'
+      'a<=b wahr wenn a kleiner gleich b'
+      'a>b wahr wenn a groesser b'
+      'a>=b wahr wenn a groesser gleich b'
+      'a!=b wahr wenn a nicht gleich b')
