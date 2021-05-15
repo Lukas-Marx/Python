804879d328a0756f7b8d7f38c98a62659225fa1b
Lukas-Marx <70298828+Lukas-Marx@users.noreply.github.com>
2021-05-15 21:39:18 +0200

    Adjusted files

--- Masterarbeit.py
+++ Masterarbeit.py
@@ -4,13 +4,17 @@
 import pandas as pd              # Bibliothek "Panel Data"
 import numpy as np               # Bibliothek "Nummerisches Python"
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
 from statsmodels.formula.api import ols
 from statsmodels.stats.diagnostic import het_white
+from statsmodels.stats.diagnostic import normal_ad
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 dataset = pd.read_excel("/Users/LukasMarx/PycharmProjects/Python/Variablen_Regression.xlsx")
 print(dataset.head())
 print(dataset.info())
-
+p_value_thresh = 0.05
 f='Social_Score~Size+SRI+Impact+ESG+Social+SIB+GBF+GBE+GBV+Environment_Score'
 reg_model= ols(formula=f, data=dataset).fit()
 print(reg_model.summary())
