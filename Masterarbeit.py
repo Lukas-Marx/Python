400ce453142d943477e8998a97fd9a12b20569ee
Lukas-Marx <70298828+Lukas-Marx@users.noreply.github.com>
2021-05-15 22:06:41 +0200

    multikollinearitätstest ergänzt.

--- Masterarbeit.py
+++ Masterarbeit.py
@@ -19,10 +19,10 @@ X = dataset[['Size','SRI','Impact','ESG','Social','SIB','GBF','GBE','GBV','Envir
 f = 'Social_Score~Size+SRI+Impact+ESG+Social+SIB+GBF+GBE+GBV+Environment_Score'
 reg_model = ols(formula=f, data=dataset).fit()
 print(reg_model.summary())
-white_test = het_white(reg_model.resid, reg_model.model.exog)
 
 #heteroskedastie
 
+white_test = het_white(reg_model.resid, reg_model.model.exog)
 labels = ['LM Statistic', 'LM-Test p-value', 'F-Statistic', 'F-Test p-value']
 print(dict(zip(labels, white_test)))
 
